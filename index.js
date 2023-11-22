50218b6934466c1ea36ece76c81fe34844dbf643
Siim Ots <siimots@gmail.com>
2023-11-22 15:12:15 +0200

    use routes to filter active stops

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Tallinn8kmBuffer = require("./buffer.json");
 const DEBUG = false;
 const USEBUFFER = true;
 
-const toGeoJSONCollection = (data) => {
+const toGeoJSONCollection = (data, withId, withRoutes) => {
   let result = { type: "FeatureCollection", features: [] };
 
   data.forEach((item) => {
@@ -17,6 +17,14 @@ const toGeoJSONCollection = (data) => {
       properties: { SiriID: SiriID, Name: name },
       geometry: { type: "Point", coordinates: [lon, lat] },
     };
+
+    if (withId) {
+      feature.properties.id = item.id;
+    }
+
+    if (withRoutes) {
+      feature.properties.routes = item.routes;
+    }
 
     result.features.push(feature);
   });
@@ -66,18 +74,22 @@ const parseCSV = (str, delimiter = ",") => {
   return arr;
 };
 
+let stops = {};
+
 const fetchStops = () => {
   let url;
   if (!DEBUG) url = "https://transport.tallinn.ee/data/stops.txt";
   else url = "http://localhost:5000/stops.txt";
+
   fetch(url)
     .then((response) => {
       return response.text();
     })
     .then((data) => {
-      const parsedData = [];
       let prev;
+
       data = parseCSV(data, ";");
+
       data.forEach((item, index) => {
         if (index > 0 && item) {
           var x = {
@@ -86,6 +98,8 @@ const fetchStops = () => {
             lat: item[2] / 1e5,
             lon: item[3] / 1e5,
             name: item[5],
+            routes: [],
+            active: false,
           };
 
           if (!x.name && prev && prev.name) {
@@ -95,19 +109,118 @@ const fetchStops = () => {
           prev = x;
 
           if (x.name && !x.name.includes("ei kasuta")) {
-            parsedData.push(x);
+            stops[x.id] = x;
           }
         }
       });
 
-      var geojson = toGeoJSONCollection(parsedData);
+      fetchRoutes();
+    });
+};
 
-      fs.writeFile("public/peatused.js", `var peatused = ${JSON.stringify(geojson)};`, (err) => {
+const fetchRoutes = () => {
+  let url;
+  if (!DEBUG) url = "https://transport.tallinn.ee/data/routes.txt";
+  else url = "http://localhost:5000/routes.txt";
+
+  fetch(url)
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      let prev;
+
+      data = parseCSV(data, ";");
+
+      data.forEach((item, index) => {
+        if (index <= 1) {
+          // 0 & 1 header rows
+          //console.log(item);
+        }
+
+        if (index >= 2 && item) {
+          var route = {
+            RouteNum: item[0],
+            RouteType: item[8],
+            Commercial: item[9],
+            RouteName: item[10],
+            RouteStops: item[13],
+          };
+
+          if (!route.RouteNum && route.RouteStops && prev && prev.RouteNum) {
+            route.RouteNum = prev.RouteNum;
+          }
+
+          if (route.RouteStops) {
+            prev = route;
+
+            let route_stops = route.RouteStops.split(",");
+
+            route_stops.forEach((stop) => {
+              if (stops[stop]) {
+                stops[stop].active = true;
+
+                stops[stop].routes.push({
+                  RouteNum: route.RouteNum,
+                  RouteName: route.RouteName,
+                  RouteType: route.RouteType,
+                  Commercial: route.Commercial,
+                });
+              } else {
+                console.error("Route has missing stop", route, stop);
+              }
+            });
+          }
+        }
+      });
+
+      const parsedData = [];
+
+      for (const stop in stops) {
+        if (stops[stop].active) {
+          parsedData.push(stops[stop]);
+        }
+      }
+
+      var old = toGeoJSONCollection(parsedData, false, false);
+
+      fs.writeFile(
+        "public/peatused.js",
+        `var peatused = ${JSON.stringify(old)};`,
+        (err) => {
+          if (err) throw err;
+        }
+      );
+      fs.writeFile("public/data.json", JSON.stringify(old, null, 2), (err) => {
         if (err) throw err;
       });
-      fs.writeFile("public/data.json", JSON.stringify(geojson, null, 2), (err) => {
-        if (err) throw err;
-      });
+
+      /*var withoutroutes = toGeoJSONCollection(parsedData, true, false);
+
+      fs.writeFile(
+        "public/stops.json",
+        JSON.stringify(withoutroutes),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+
+      var withroutes = toGeoJSONCollection(parsedData, true, true);
+
+      fs.writeFile(
+        "public/routes.js",
+        `var peatused = ${JSON.stringify(withroutes)};`,
+        (err) => {
+          if (err) throw err;
+        }
+      );
+      fs.writeFile(
+        "public/routes.json",
+        JSON.stringify(withroutes, null, 2),
+        (err) => {
+          if (err) throw err;
+        }
+      );*/
     });
 };
 
